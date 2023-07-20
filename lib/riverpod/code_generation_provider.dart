f81f0d8bed45c28542766dff13b00cccbe2bb5d0
Soohyeon.Hwang <soohyeon@directional.net>
2023-07-20 12:07:38 +0900

    riverpod code generation

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -7,8 +7,63 @@ part 'code_generation_provider.g.dart';
 final _testProvider = Provider<String>((ref) => 'Hello Code Generation');
 
 @riverpod
-String gState(GStateRef ref){
+String gState(GStateRef ref) {
   return 'Hello Code Generation';
 }
 
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
+@Riverpod(
+  // 살려둬라
+  keepAlive: true,
+)
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
 // 2) parameter > Family 파라미터를 일반 함수처럼 사용할 수 있도록
+class Parameter {
+  final int number1;
+  final int number2;
+
+  Parameter({
+    required this.number1,
+    required this.number2,
+  });
+}
+
+final _testFamilyProvider = Provider.family<int, Parameter>(
+    (ref, parameter) => parameter.number1 * parameter.number2);
+
+@riverpod
+int gStateMultiply(
+  GStateMultiplyRef ref, {
+  required int number1,
+  required int number2,
+}) {
+  return number1 * number2;
+}
+
+@riverpod
+class GStateNotifier extends _$GStateNotifier{
+
+  @override
+  int build(){
+    return 0;
+  }
+
+  increment(){
+    state++;
+  }
+
+  decrement(){
+    state--;
+  }
+}
--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -8,14 +8,99 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+
     final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
+    final state4 = ref.watch(gStateMultiplyProvider(
+      number1: 10,
+      number2: 20,
+    ));
+
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text('State1: $state1'),
+          state2.when(
+            data: (data) {
+              return Text(
+                'state2: $data',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          state3.when(
+            data: (data) {
+              return Text(
+                'state3: $data',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          Text('state4: $state4'),
+          Consumer(
+            builder: (context, ref, child) {
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('state5: $state5'),
+                  child!,
+                ],
+              );
+            },
+            child: Text('hello'),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).increment();
+                },
+                child: Text('Increment'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).decrement();
+                },
+                child: Text('Decrement'),
+              ),
+            ],
+          ),
+          // invalidate()
+          // 유효하지 않게 하다
+          ElevatedButton(
+            onPressed: () {
+              ref.invalidate(gStateNotifierProvider);
+            },
+            child: Text('Invalidate'),
+          ),
         ],
       ),
     );
+  }
+}
+
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+    return Text('state5: $state5');
   }
 }
